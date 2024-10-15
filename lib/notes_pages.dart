59ca101f1cf7bdda0535821bda55d4238063ef40
omarmushtaha2525gmail.com <omarmushtaha37@gmail.com>
2024-10-15 13:16:22 +0300

    build add and delete Function in the note page

--- lib/notes_pages.dart
+++ lib/notes_pages.dart
@@ -1,10 +1,39 @@
-
 import 'package:flutter/material.dart';
-class NotesPage extends StatelessWidget {
-  const NotesPage({super.key});
+import 'package:hive_flutter/hive_flutter.dart';
+class NotesPage extends StatefulWidget {
+  const NotesPage({Key? key}) : super(key: key);
+
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  final TextEditingController _noteController = TextEditingController();
+  late Box notesBox;
+
+  @override
+  void initState() {
+    super.initState();
+    notesBox = Hive.box('notes');
+  }
+
+  void _addNote() {
+    if (_noteController.text.isNotEmpty) {
+      notesBox.add(_noteController.text);
+      _noteController.clear();
+      setState(() {});
+    }
+  }
+
+  void _deleteNote(int index) {
+    notesBox.deleteAt(index);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
