dac30dc2759509f8d628f7d19ba33c2914c1898f
omarmushtaha2525gmail.com <omarmushtaha37@gmail.com>
2024-10-15 13:06:36 +0300

    init hive in the main and open box of hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_hive_app/notes_pages.dart';
 
-void main(){
-  runApp(MyApp());
+void main()async{
+  await Hive.initFlutter();
+  await Hive.openBox('notes'); // Open a Hive box for notes
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: NotesPage(),
     );
   }
